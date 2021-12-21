046a1283520ba28b7208c8d4ec1bac50107ada2a
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2021-12-21 15:57:41 +0500

    added unit tests

---
+++ tests/test_case1.py
@@ -0,0 +1,12 @@
+import pytest
+
+l = [1,2,3,4,5,6]
+
+
+def test_case():
+    assert(l[0] == 1)
+    assert(l[1] == 2)
+    assert(l[2] == 3)
+    assert(l[3] == 4)
+    assert(l[4] == 5)
+    assert(l[5] == 6)
